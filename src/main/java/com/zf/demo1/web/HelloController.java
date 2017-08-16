a8f3a42261adda658d2a4be9689ebe01ba785dd9
zhangfeng761 <zhangfeng761@163.com>
2017-08-17 02:17:18 +0800

    Spring boot的基础项目，实现一个输出“Hello World” 的RESTful的接口以及针对该接口的单元测试用例。 Spring boot 1.5.6 maven 3.3.9 jdk 1.8

---
+++ src/main/java/com/zf/demo1/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.zf.demo1.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**RestFul接口
+ * Created by ZF on 2017/08/16.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index(){
+        return "Hello World";
+    }
+}
