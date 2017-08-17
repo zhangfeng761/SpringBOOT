928c1c73ce0e21476a931cf1795161e4c7066ee0
zhangfeng761 <zhangfeng761@163.com>
2017-08-17 10:42:34 +0800

    分支测试

--- src/main/java/com/zf/demo1/web/HelloController.java
+++ src/main/java/com/zf/demo1/web/HelloController.java
@@ -3,7 +3,7 @@ package com.zf.demo1.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**输CHU RestFul接口
+/**输出Hello World的RestFul接口
  * Created by ZF on 2017/08/16.
  */
 @RestController
